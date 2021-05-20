16b55b8ebd63db50f2b3031fd33cde4de1188159
yamaimo <yarnaimo@gmail.com>
2021-05-20 22:30:49 +0900

    color saturation

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,13 +1,13 @@
 import { hsl } from '../utils/css'
 
 export const appPalette = {
-  music: { background: hsl(346, 83, 97)(), color: hsl(346, 97, 62)() },
+  music: { background: hsl(346, 87, 97)(), color: hsl(346, 97, 62)() },
   dev: { background: hsl(40, 97, 95)(), color: hsl(40, 97, 53)() },
   other: { background: hsl(169, 70, 95)(), color: hsl(169, 70, 53)() },
 
-  twitter: { background: hsl(204, 77, 96)(), color: hsl(204, 88, 53)() },
+  twitter: { background: hsl(204, 81, 96)(), color: hsl(204, 88, 53)() },
   github: { background: hsl(0, 0, 96)(), color: hsl(0, 0, 10)() },
-  qiita: { background: hsl(94, 61, 94)(), color: hsl(94, 100, 39)() },
+  qiita: { background: hsl(94, 65, 94)(), color: hsl(94, 100, 39)() },
   zenn: { background: hsl(207, 83, 96)(), color: hsl(207, 100, 62)() },
 }
 
